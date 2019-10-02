99699b8a58e86b5042078418cf7ce57b9f620a19
Noble Obioma <nobioma1@gmail.com>
2019-10-02 15:48:57 +0100

    Create method to set and get current room

--- src/player.py
+++ src/player.py
@@ -8,3 +8,8 @@ class Player:
   def __str__(self):
     return f"Name: {self.name} \nCurrent Room: {self.current_room}"
 
+  def get_current_room(self):
+    return self.current_room
+  
+  def set_current_room(self, room):
+    self.current_room = room
