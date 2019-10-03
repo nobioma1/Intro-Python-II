e80528b490bb2ad9a26bc8647dad978422db8cf5
Noble Obioma <nobioma1@gmail.com>
2019-10-03 09:06:43 +0100

    Add direction attributes to Room

--- src/room.py
+++ src/room.py
@@ -4,6 +4,13 @@ class Room:
   def __init__(self, name, description):
     self.name = name
     self.description = description
+    self.n_to = None  
+    self.s_to = None
+    self.e_to = None
+    self.w_to = None 
 
+  def get_room(self):
+    return self.name
+  
   def __str__(self):
     return f"Room Name: {self.name} \nRoom Description: {self.description}"
