c3332406bd6c009277cf9d9d9cd4ede05904ad52
Noble Obioma <nobioma1@gmail.com>
2019-10-03 09:08:34 +0100

    Pass an Instance on Outside room class instead on the key

--- src/adv.py
+++ src/adv.py
@@ -39,7 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player('Lupita', 'outside')
+player = Player('Lupita', room['outside'])
 
 # Write a loop that:
 #
