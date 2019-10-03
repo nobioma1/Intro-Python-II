4d50745a36ca9550fde11eab0a4b1d277ea068e6
Noble Obioma <nobioma1@gmail.com>
2019-10-03 16:49:02 +0100

    Add method to add item, remove item and get room name

--- src/room.py
+++ src/room.py
@@ -4,13 +4,13 @@
 
 class Room:
     def __init__(self, name, description, room_items=[]):
-    self.name = name
-    self.description = description
+        self.name = name
+        self.description = description
         self.room_items = room_items
-    self.n_to = None  
-    self.s_to = None
-    self.e_to = None
-    self.w_to = None 
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def __str__(self):
         return f" Room Name: {self.name}\n Room Description: {self.description}"
@@ -23,7 +23,12 @@ class Room:
             for index, item in enumerate(self.room_items):
                 items += f" {index + 1}. {item}\n"
         return items
-    return self.name
-  
-  def __str__(self):
-    return f"Room Name: {self.name} \nRoom Description: {self.description}"
+
+    def add_room_item(self, item):
+        self.room_items.append(item)
+
+    def remove_room_item(self, item_index):
+        self.room_items.pop(item_index)
+
+    def get_name(self):
+        return self.name
