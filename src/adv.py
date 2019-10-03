58b0a89c1c6e5298bde16f082dce058fbf1c1566
Noble Obioma <nobioma1@gmail.com>
2019-10-03 16:50:36 +0100

    Linting and fixes

--- src/adv.py
+++ src/adv.py
@@ -22,7 +22,7 @@ room = {
 
     'overlook': Room("Grand Overlook",
                      """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
+                    into the darkness. Ahead to the north, a light flickers in
                     the distance, but there is no way across the chasm.
                     """,
                      [Item("lamp", "When the light flickers")]),
@@ -34,8 +34,8 @@ into the darkness. Ahead to the north, a light flickers in
 
     'treasure': Room("Treasure Chamber",
                      """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+                    chamber! Sadly, it has already been completely emptied by
+                    earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -50,58 +50,66 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
 # Main
-#
+
 
 # Make a new player object that is currently in the 'outside' room.
+
 player = Player('Lupita', room['outside'])
 
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
-
-print(
-    """
-        **Welcome To Adventure Room Game**
-            Possible Directions To Go:
-                n - North
-                s - South
-                e - East
-                w - West 
-    """
-)
 
 while True:
+    print("\n\t**Command LineAdventure Room Game**")
+
+    # * Prints the current room name
+    # * Prints the current description (the textwrap module might be useful here).
+
     player_current_room = player.get_current_room()
     print(f"\nCurrent Room:\n{player_current_room}")
 
-    user_input = input("\nIn what direction do you want to go now? \n > ").lower()
+    # * Print Current room Items
+
+    current_room_items = player_current_room.view_room_items()
+    print(current_room_items)
+
+    # * Waits for user input and decides what to do.
+
+    user_input = input(
+        """What do you want to do now? 
+        (n - North, s - South, e - East, w - West, i - Your Inventory, q - Quit)
+        (take <ITEM_NAME> - Pick Item, drop <ITEM_NAME> - Drop Item )
+    > """).lower()
+
+    # If the user enters a cardinal direction, attempt to move to the room there.
 
     if user_input == "n":
         next_room = player_current_room.n_to
-        player.set_player_room(next_room)
+        player.move_player(next_room)
     elif user_input == "s":
         next_room = player_current_room.s_to
-        player.set_player_room(next_room)
+        player.move_player(next_room)
     elif user_input == "e":
         next_room = player_current_room.e_to
-        player.set_player_room(next_room)
+        player.move_player(next_room)
     elif user_input == "w":
         next_room = player_current_room.w_to
+        player.move_player(next_room)
+    elif user_input == "i":
+        inventories = player.view_inventory()
+        print(inventories)
     elif 'drop' in user_input:
         print(player.drop_item(user_input))
     elif 'take' in user_input:
         print(player.take_item(user_input))
     elif user_input == "q":
+
+        # If the user enters "q", quit the game.
+
         print("Bye! See you Later")
         break
     else:
+
+        # Print an error message if the movement isn't allowed.
+
         print(f"-->Movement {user_input} is not allowed<--\n")
