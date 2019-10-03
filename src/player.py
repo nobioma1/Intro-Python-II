2b21a5df11a7d0c78239c21f6ac3c599659c3e3c
Noble Obioma <nobioma1@gmail.com>
2019-10-03 16:43:28 +0100

    Create Player method to move player to another room

--- src/player.py
+++ src/player.py
@@ -1,18 +1,24 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
 class Player:
   def __init__(self, name, current_room):
     self.name = name
     self.current_room = current_room
+        self.inventory = []
   
   def __str__(self):
-    return f"Name: {self.name} \nCurrent Room: {self.current_room}"
+        return f"\tName: {self.name} \tCurrent Room: {self.current_room}"
 
   def get_current_room(self):
     return self.current_room
   
   def set_player_room(self, room):
-    if room != None:
       self.current_room = room
+
+    def move_player(self, room):
+        if room != None:
+            self.set_player_room(room)
     else:
       print("-->Sorry, you cannot move in this direction in your current room<--")
