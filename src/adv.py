d87b8c2a5c0a0808323761256a695693df085f01
Noble Obioma <nobioma1@gmail.com>
2019-10-03 16:50:10 +0100

    Add items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,23 +1,39 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     [
+                         Item("spoon", "Use to take soup"),
+                         Item("clock", "Know the time at the moment, time is money")
+                     ]),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer",
+                     "Dim light filters in from the south. Dusty passages run north and east.",
+                     [
+                         Item("book", "Write your experience"),
+                         Item("mask", "For the dusty air in the passage"),
+                         Item("blanket", "For the cold nights")
+                     ]),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook",
+                     """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+                    the distance, but there is no way across the chasm.
+                    """,
+                     [Item("lamp", "When the light flickers")]),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage",
+                     """The narrow passage bends here from west
+                    to north. The smell of gold permeates the air.""",
+                     [Item("bag", "Carry you items")]),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber",
+                     """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
